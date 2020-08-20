1bdcceaaf8dc49a9bb06aeee5ef3922d0a92b0d3
Gabriel <joaogabrielonofre@gmail.com>
2020-08-20 15:04:48 -0300

    Create new page Encomendas

---
+++ pages/encomendas.js
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar';
+import Info from '../components/Info';
+import Footer from '../components/Footer';
+
+const Encomendas = () => {
+  return (
+    <>
+      <Navbar />
+      <Info isCatalog={true} />
+      <Footer />
+    </>
+  );
+};
+
+export default Encomendas;
